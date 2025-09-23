1e701d6c89508f6e64f064fb6a2ce1c91d8ef01a
GuiAugustinho <gm.dasilva007@gmail.com>
2025-09-23 12:37:20 -0300

    atualização

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 
-import pkg, { Prisma } from '@prisma/client';
-const {PrismaClient} = pkg;
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
 const app = express()
 
 app.use(express.json())
-
-
+app.use(cors())
 
 //Criar as rotas
 
 app.put('/cadastro/:id', async (req,res)=>{
-
     //console.log(req.params.id)
     await prisma.usuario.update({
         where:{
@@ -24,39 +23,36 @@ app.put('/cadastro/:id', async (req,res)=>{
             nome: req.body.nome,
             idade: req.body.idade
         }
-        
-
     })
-    res.status(201).json({"message":"Usuario Atualizado"})
+    res.status(201).json({"message":"Usuário Atualizado"})
+})
+
+app.delete('/cadastro/:id', async (req,res)=>{
+    //console.log(req.params.id)
+    await prisma.usuario.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+    res.status(200).json({"message":"Usuário Excluído"})
+})
+
+app.get('/cadastro', async (req, res) => {
+    const lista_usuarios = await prisma.usuario.findMany()
+    res.status(200).json(lista_usuarios)
 })
 
 app.post('/cadastro', async (req, res) => {
     await prisma.usuario.create({
         data:{
-            nome: req.body.nome,
             email: req.body.email,
+            nome: req.body.nome,
             idade: req.body.idade
         }
-    })  
-    res.status(201).json(req.body)   
-})
-   
-app.get('/cadastro',async (req,res)=>{
-    const lista_usuarios = await prisma.usuario.findMany()
-    res.status(200).json(lista_usuarios)
-})
-app.delete('/cadastro/:id', async (req,res)=>{
-
-    //console.log(req.params.id)
-    await prisma.usuario.delete({
-        where:{
-            id:req.params.id
-        },
     })
-    res.status(201).json({"message":"Remove"})
+    res.status(201).json(req.body)
 })
 
 //Configurar porta do servidor
 
 app.listen(3000, () => { console.log('SERVIDOR RODANDO!') })
-
